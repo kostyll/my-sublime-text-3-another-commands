786e099ababf201d2bb159b29729ea9f6d0d45d0
Andrew <andrew@vasyltsiv.esx.ua>
2014-10-15 16:02:54 +0300

    requests-lib and ssl support for plugin added

--- plugin.py
+++ plugin.py
@@ -1,9 +1,36 @@
 import sublime
 import sublime_plugin
 
+import os
+import sys
+
+root_location = os.path.dirname(__file__)
+
+requests_path = os.path.join(root_location,'requests')
+sublimessl = os.path.join(root_location,'sublimessl')
+
+sys.path.insert(0, requests_path)
+sys.path.insert(0, sublimessl)
+
+from SSL import ssl
+
 def plugin_loaded():
     # print ("{} loaded".format(__file__))
-    pass
+    from imp import reload
+    # NEED TO DO SOME MONKEY PATCHING ACTIONS
+    # reload(somelib.somemodule)
+    # somelib.somemodule.ssl = ssl
+    # For e.g.:
+    # reload(yandex_translate)
+    # reload(yandex_translate.requests)
+    # reload(yandex_translate.requests.packages.urllib3.connection)
+    # yandex_translate.requests.packages.urllib3.connection.ssl = ssl
+
+    # func = yandex_translate.requests.packages.urllib3.response.HTTPResponse.stream
+    # def urllib3_HTTPResponse_stream_wrapper(func):
+    #     return lambda self,amt=None,decode_content=None:func(self,None,decode_content)
+    # yandex_translate.requests.packages.urllib3.response.HTTPResponse.stream = urllib3_HTTPResponse_stream_wrapper(func)
+    # print ("Plugin RussianVariableTranslate is loaded")
 
 class ItemizeLatexStringsCommand(sublime_plugin.TextCommand):
     """
