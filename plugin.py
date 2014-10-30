edd9fda1e7c1e97b6a84ebe44b6c252c8649dfbd
Andrew <andrew.freelance@i.ua>
2014-10-30 17:19:59 +0200

    json-like data parser implementations was been started

--- plugin.py
+++ plugin.py
@@ -1,16 +1,34 @@
 import sublime
 import sublime_plugin
 
+# TODO: json-like data parsing neet to be implemented
+
 import os
 import sys
+import pprint
 
 root_location = os.path.dirname(__file__)
+
+third_party_path = os.path.join(root_location,'thirdparty')
 
 requests_path = os.path.join(root_location,'requests')
 sublimessl = os.path.join(root_location,'sublimessl')
 
+pyparsing_path = os.path.join(third_party_path,'pyparsing/src')
+
 sys.path.insert(0, requests_path)
 sys.path.insert(0, sublimessl)
+sys.path.insert(0, pyparsing_path)
+
+import pyparsing
+# from pyparsing import *
+from pyparsing import (
+    Word, Literal,
+    alphas, nums, alphanums,
+    OneOrMore, ZeroOrMore,
+    Or, And,
+    Forward, Optional, Group,Suppress
+)
 
 from SSL import ssl
 
@@ -32,6 +50,31 @@ def plugin_loaded():
     # yandex_translate.requests.packages.urllib3.response.HTTPResponse.stream = urllib3_HTTPResponse_stream_wrapper(func)
     # print ("Plugin RussianVariableTranslate is loaded")
 
+# rus_alphas = 'їійцукенгшщзхъфывапролджэячсмитьбюІЇЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
+rus_alphas = ""
+comma = ','
+
+string = Word(alphas+rus_alphas+nums+ alphanums)
+quoted_string = Or(string,Or('"' + string + '"',"'"+string + "'"))
+
+number = Word(nums+'.,')
+
+value = Forward()
+member = Forward()
+array = Forward()
+dict_ = Forward()
+
+elements = OneOrMore(value)
+members = OneOrMore(member)
+
+member << quoted_string+Suppress(':')+ value
+value << (string|quoted_string|number|array|dict_)
+array << Suppress("[") + Optional(elements) + Suppress("]")
+
+dict_ << Suppress("{") + Optional(members) + Suppress("}")
+
+
+
 class ItemizeLatexStringsCommand(sublime_plugin.TextCommand):
     """
     Replaces every selected line with prefix '\\item{' and suffix '}'
@@ -49,3 +92,26 @@ class ItemizeLatexStringsCommand(sublime_plugin.TextCommand):
                     result_text.append(result_line)
                 # print (result_text)
                 self.view.run_command("insert",{"characters":"\n".join(result_text)})
+
+class PretyDictPrinter(object):
+    def __init__(self,text):
+        self.text = text
+
+    def print_pretty(self):
+        text = self.text
+        results = dict_.parseString(text)
+        pprint.pprint(results)
+
+
+class PretifyDictDataCommand(sublime_plugin.TextCommand):
+    """
+    Reprints text from ugly view to pretty
+    """
+    def run(self,edit):
+        for region in self.view.sel():
+            if not region.empty():
+                text = self.view.substr(region)
+                prety_printer = PretyDictPrinter(text)
+                prety_text = prety_printer.print_pretty()
+
+                # self.view.run_command("insert",{"characters":prety_text})
